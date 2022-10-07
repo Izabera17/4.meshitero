4c134161abf87f054b6eac119c1a7eae2cebf78b
Izabera17 <106213752+Izabera17@users.noreply.github.com>
2022-10-07 23:11:25 +0000

    [Finish]4-8 画像投稿機能に必要なモデルを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :config_permitted_parameters, if: :devise_controller?
   
-  protected
+    def after_sign_in_path_for(resourse)
+      about_path
+    end
+    
+    def after_sign_out_path_for(resourse)
+      about_path
+    end
+    
+    protected
   
-  def config_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+    def config_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
